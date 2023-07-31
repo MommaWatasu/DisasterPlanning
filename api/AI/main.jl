e84a71a94a1826288b60814e00669eb03b16e548
MommaWatasu <ascendwatson@gmail.com>
2023-07-31 16:56:15 -0700

    make API available with Genie and update models

--- api/AI/main.jl
+++ api/AI/main.jl
@@ -1,6 +1,9 @@
 # import the libraris for hosting API
-using Genie, Genie.Renderer.Json
+using Genie, Genie.Requests, Genie.Renderer.Json
+using HorseML.Preprocessing
+using HorseML.Preprocessing: transform, inv_transform
 using Dates
+using Dates: day
 # import the libraries for AI
 using Flux
 using HorseML.Preprocessing
@@ -9,22 +12,27 @@ using BSON: @load
 
 function predict(data)
     # load the model and scaler
-    @load "./scaler.bson" scaler
+    @load "./xscaler.bson" xscaler
+    @load "./tscaler.bson" tscaler
     @load "./model.bson" model
     # predict
-    return inv_transform(scaler, model(transform(scaler)))
+    println(model(transform(xscaler, data)'))
+    return inv_transform(tscaler, model(transform(xscaler, data)')')
 end
 
 # route for providing API
-route("/earthquake.json", method=GET) do
-    json_data = jsonpayload()["last"]
-    model_data = [json_data["time"], json_data["mag"], json_data["lat"], json_data["long"], json_data["depth"]]
-    hour = predict(model_data)
-    if hour-json_data["time"] < 0
-        day = day(now()) + 1
-    else
-        day = day(now())
+route("/earthquake.json", method=POST) do
+    json_data = jsonpayload()
+    earthquake_data = json_data["last"]
+    model_data = [earthquake_data["time"], earthquake_data["mag"], earthquake_data["lat"], earthquake_data["long"], earthquake_data["depth"]]
+    model_data = hcat(model_data, model_data)'
+    hour = predict(model_data)[1, 1]
+    pday = day(now())
+    if hour-earthquake_data["time"] < 0
+        pday = day(now()) + 1
     end
-    data = Dict("data"=>day, "hour"=>hour, "id"=>json_data["jobID"])
+    data = Dict("data"=>pday, "hour"=>hour, "id"=>json_data["jobID"])
     json(data)
 end
+
+up(4000, async=false)
