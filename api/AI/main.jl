1c90c8e2b6a79cc82a246cbd5c0979dd5684b6eb
MommaWatasu <ascendwatson@gmail.com>
2023-08-01 16:15:34 -0700

    update model and API

--- api/AI/main.jl
+++ api/AI/main.jl
@@ -10,31 +10,48 @@ using BSON: @load
 
 function predict(data)
     # load the model and scaler
-    @load "./main-model/xscaler.bson" xscaler
-    @load "./main-model/tscaler.bson" tscaler
-    @load "./main-model/model.bson" model
+    @load "./main_model/xscaler.bson" xscaler
+    @load "./main_model/tscaler.bson" tscaler
+    @load "./main_model/model.bson" model
     # predict
-    main_predict = inv_transform(tscaler, model(transform(xscaler, data)')')
-    @load "./day-model/xscaler.bson" xscaler
-    @load "./day-model/tscaler.bson" tscaler
-    @load "./day-model/model.bson" model
+    main_predict = inv_transform(tscaler, model(transform(xscaler, vcat(data[1:2], data[5])')')')
+    @load "./location_model/xscaler.bson" xscaler
+    @load "./location_model/tscaler.bson" tscaler
+    @load "./location_model/model.bson" model
     # predict
-    day_predict = inv_transform(tscaler, model(transform(xscaler, data)')')
-    return hcat(day_predict, main_predict)
+    location_predict = inv_transform(tscaler, model(transform(xscaler, data[3:5]')')')
+    return hcat(main_predict, location_predict)
 end
 
 # route for providing API
 route("/earthquake.json", method=POST) do
     json_data = jsonpayload()
     earthquake_data = json_data["last"]
-    model_data = [earthquake_data["month"], earthquake_data["day"], earthquake_data["time"], earthquake_data["mag"], earthquake_data["lat"], earthquake_data["long"], earthquake_data["depth"]]
-    model_data = hcat(model_data, model_data)'
+    model_data = [earthquake_data["time"], earthquake_data["mag"], earthquake_data["lat"], earthquake_data["long"], earthquake_data["depth"]]
     output = predict(model_data)
+    hour = earthquake_data["time"] + Int(round(output[1]))
+    day = earthquake_data["day"]
     month = earthquake_data["month"]
-    if Int(round(output[1])) - earthquake_data["day"] < 0
-        month += 1
+    if hour > 24
+        day += 1
+        hour -= 24
+        if month in [1, 3, 5, 7, 8, 10, 12] && day > 31
+            day = 1
+            month += 1
+        elseif day > 30
+            day = 1
+            month += 1
+        end
     end
-    data = Dict("month"=>month, "day"=>Int(round(output[1])), "hour"=>Int(round(output[2])), "mag"=>round(output[3]*100)/100, "id"=>json_data["jobID"])
+    data = Dict(
+        "month"=>month,
+        "day"=>day,
+        "hour"=>hour,
+        "mag"=>round(output[2]*100)/100,
+        "lat"=>round(output[3]*100)/100,
+        "lon"=>round(output[4]*100)/100,
+        "id"=>json_data["jobID"]
+        )
     json(data)
 end
 
