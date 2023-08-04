35a5eeed4db5aa9f33fcc756e676086ad134b81c
Wes Griffin <43881864+Wesius@users.noreply.github.com>
2023-08-03 21:53:34 -0700

    final push for the night

--- api/api.js
+++ api/api.js
@@ -8,6 +8,9 @@ const ejs = require('ejs');
 const fs = require('fs');
 const store = require('store');
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 const userAgentParser = require("user-agent");
 var admin = require("firebase-admin");
 
@@ -18,7 +21,7 @@ let prepareData = require('../data/prepare.json');
 var serviceAccount = require("E:/service-account.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
@@ -28,6 +31,8 @@ Userdb.doc("XuVCOg3Nk03dxo3uDqmM").get().then(async (doc) => {
     console.log(await doc.data())
 });
 */
+
+
 
 const GithubActionURL = "https://api.github.com/repos/MommaWatasu/DisasterPlanning/dispatches";
 const Token = "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";
@@ -64,23 +69,23 @@ const header = {
     "Authorization": "Bearer " + Token
 }
 
-async function getUser(id){
+async function getUser(id) {
     var user = await Userdb.doc(id).get();
-    if (!user.exists){
+    if (!user.exists) {
         return null;
-    }else{
+    } else {
         return user.data();
     }
 }
 
-async function validateUser(id, token){
+async function validateUser(id, token) {
     var user = await getUser(id);
-    if (user == null){
+    if (user == null) {
         return false;
-    }else{
-        if (user.AccessTokens.indexOf(token)){
+    } else {
+        if (user.AccessTokens.indexOf(token)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
@@ -101,9 +106,9 @@ planCache = JSON.parse(fs.readFileSync('../data/planCache.json', 'utf8'));
 app.set('trust proxy', true);
 app.set('view engine', 'ejs');
 
-async function updateCache(){
-    for (var job in jobCache){
-        if (job.time < Date.now() - 600000){
+async function updateCache() {
+    for (var job in jobCache) {
+        if (job.time < Date.now() - 600000) {
             delete jobCache[job];
         }
     }
@@ -115,21 +120,21 @@ async function updateCache(){
 }
 setTimeout(updateCache, 30000);
 
-async function getIPInfo(ip){
+async function getIPInfo(ip) {
     console.log(ip)
     if (ip.startsWith(":")) {
         ip = "128.12.123.204";
     }
     var info = await axios.get('https://ipapi.co/' + ip + '/json/');
-     return info.data;
+    return info.data;
 }
 
 const createRandomString = (length) => {
     const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
     let randomString = '';
     for (let i = 0; i < length; i++) {
-      const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
-      randomString += randomCharacter;
+        const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
+        randomString += randomCharacter;
     }
     return randomString;
 };
@@ -140,11 +145,6 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.redirect('/app');
-});
-
-app.get('/api', (req, res) => {
-    var indexHtmlPath = path.join(__dirname, '..', 'webapp', 'api.html');
-    res.sendFile(indexHtmlPath);
 });
 
 app.get('/style.css', (req, res) => {
@@ -162,20 +162,22 @@ app.get('/CDN/images/:image', (req, res) => {
     const imagePath = path.join(__dirname, '..', 'webapp', 'assets', 'images', image);
     try {
         res.sendFile(imagePath);
-    }
-    catch {
+    } catch {
         res.send('Image not found');
     }
 });
 
 app.get('/bard', (req, res) => {
-    res.render('bardbad', { what: 'an ai', who: 'bard' });
-  });
+    res.render('bardbad', {
+        what: 'an ai',
+        who: 'bard'
+    });
+});
 
 app.get('/api/clientlocation', async (req, res) => {
     var ip = await req.ip;
     console.log(ip);
-    if (ip == "::1"){
+    if (ip == "::1") {
         ip = "128.12.123.204"
     }
     axios.get('https://ipapi.co/' + ip + '/json/').then((response) => {
@@ -187,32 +189,45 @@ app.get('/api/clientlocation', async (req, res) => {
             "country": response.data.country
         });
         //res.json(response.data);
-    }
-    ).catch((error) => {
+    }).catch((error) => {
         console.log(error);
     });
     // https://ipapi.co/128.12.123.204/json/
 });
 
 app.get('/app', async (req, res) => {
-    var locals = {
-    };
+    var locals = {};
+    if (req.cookies.userid == undefined || req.cookies.sessiontoken == undefined) {
+        locals.user = "Not Logged In"
+    } else {
+        var user = await getUser(req.cookies.userid);
+        if (user == null) {
+            locals.user = "Not Logged In"
+        } else {
+            if (user.AccessTokens.indexOf(req.cookies.sessiontoken) != -1) {
+                locals.user = user.Name.First + " " + user.Name.Last;
+            } else {
+                locals.user = "Not Logged In"
+            }
+        }
+    }
+    console.log(req.cookies)
 
     var ip = await req.ip;
     console.log(ip);
     if (ip.startsWith(":")) {
         ip = "128.12.123.204";
     }
-    
-    if (ip == "128.12.123.204"){
+
+    if (ip == "128.12.123.204") {
         locals.city = "Stanford";
         locals.region = "CA";
         locals.longitude = "-122.1639";
         locals.latitude = "37.423";
         locals.response = false
-    }else{
+    } else {
         var ipInfo = await getIPInfo(ip);
-        if (ipInfo.city){
+        if (ipInfo.city) {
             locals.city = ipInfo.city;
             locals.region = ipInfo.region_code;
             locals.longitude = ipInfo.longitude;
@@ -226,7 +241,11 @@ app.get('/app', async (req, res) => {
     } else {
         res.render('new/mobile/home.ejs', locals);
     }
-   
+
+});
+
+app.get("/api/login", async (req, res) => {
+    res.render('new/login.ejs');
 });
 
 app.get("/api/ai/locations.png", (req, res) => {
@@ -234,15 +253,14 @@ app.get("/api/ai/locations.png", (req, res) => {
 });
 
 app.get("/api/ai/risk_images/:jobId", (req, res) => {
-    if(fs.existsSync(`../api/ai/risk_images/${req.params.jobId}.png`)){
+    if (fs.existsSync(`../api/ai/risk_images/${req.params.jobId}.png`)) {
         res.sendFile(path.join(__dirname, "..", "api", "ai", "risk_images", `${req.params.jobId}.png`));
-    }else{
+    } else {
         res.sendFile("../api/ai/locations.png");
     }
 });
 
 app.get('/app/risk', async (req, res) => {
-
     var locals = {
         "jobID": createRandomString(5),
     };
@@ -252,16 +270,16 @@ app.get('/app/risk', async (req, res) => {
     if (ip.startsWith(":")) {
         ip = "128.12.123.204";
     }
-    
-    if (ip == "128.12.123.204"){
+
+    if (ip == "128.12.123.204") {
         locals.city = "Stanford";
         locals.region = "CA";
         locals.longitude = "-122.1639";
         locals.latitude = "37.423";
         locals.response = false
-    }else{
+    } else {
         var ipInfo = await getIPInfo(ip);
-        if (ipInfo.city){
+        if (ipInfo.city) {
             locals.city = ipInfo.city;
             locals.region = ipInfo.region_code;
             locals.longitude = ipInfo.longitude;
@@ -276,13 +294,11 @@ app.get('/app/risk', async (req, res) => {
         "long": Number(locals.longitude)
     }
 
-    console.log(body)
-
     var risk;
 
-    try{
+    try {
         risk = await axios.post("http://localhost:4000/risk.json", body)
-    }catch{
+    } catch {
         console.log("Error")
         risk = {}
         risk.data = {
@@ -290,21 +306,21 @@ app.get('/app/risk', async (req, res) => {
         }
     }
 
-    switch(risk.data.level){
+    switch (risk.data.level) {
         case 1:
             locals.rating = "Low Risk";
             locals.color = "green";
-        break;
+            break;
 
         case 2:
             locals.rating = "Medium Risk";
             locals.color = "orange";
-        break;
+            break;
 
         case 3:
             locals.rating = "High Risk";
             locals.color = "rgb(212, 46, 34)";
-        break;
+            break;
     }
 
     var currentDate = new Date();
@@ -312,21 +328,43 @@ app.get('/app/risk', async (req, res) => {
     var lastEarthquakes;
     var worked = false;
     var count = 0;
-    while(worked == false){
-        try{
-                lastEarthquakes = await axios.get(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=37.423&longitude=-122.1639&minmagnitude=2&mindepth=0&maxdepth=1000&starttime=${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}&endtime=${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}&maxradiuskm=180`)
-                count++;
-                if (lastEarthquakes.data.features.length > 0){
-                    worked = true;
-                    console.log(count)
-                    break;
-                }
-        }catch{
+    console.log("Government is working :|")
+    while (worked == false) {
+        try {
+            lastEarthquakes = await axios.get(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&latitude=37.423&longitude=-122.1639&minmagnitude=2.5&mindepth=0&maxdepth=1000&starttime=2023-${currentDate.getMonth()}-${currentDate.getDate()}&endtime=2023-${currentDate.getMonth() + 1}-${currentDate.getDate()}&maxradiuskm=180`)
+            count++;
+            if (lastEarthquakes.data.features.length > 0) {
+                worked = true;
+                console.log(count)
+                break;
+            }
+        } catch {
             console.log("Waste of $1.7B tax payer dollars")
         }
     }
-    
+
     const recent = lastEarthquakes.data.features[0]
+    var recents = [];
+
+    for (i = 0; i < 4; i++) {
+        var time = new Date(lastEarthquakes.data.features[i].properties.time)
+        recents.push({
+            "time": {
+                "hour": time.getHours(),
+                "day": time.getDate(),
+                "month": Months[time.getMonth() + 1]
+            },
+            "mag": lastEarthquakes.data.features[i].properties.mag,
+            "lat": lastEarthquakes.data.features[i].geometry.coordinates[1],
+            "long": lastEarthquakes.data.features[i].geometry.coordinates[0],
+            "depth": lastEarthquakes.data.features[i].geometry.coordinates[2],
+            "place": lastEarthquakes.data.features[i].properties.place
+        })
+    }
+
+    locals.recents = recents;
+
+    var earthquakeTime = new Date(recent.properties.time);
 
     var body2 = {
         "name": "earthquake",
@@ -338,13 +376,13 @@ app.get('/app/risk', async (req, res) => {
         "status": "pending",
         "jobID": locals.jobID,
         "last": {
-            "time": await new Date(recent.properties.time).getHours,
+            "time": earthquakeTime.getHours(),
             "mag": recent.properties.mag,
             "lat": recent.geometry.coordinates[1],
             "long": recent.geometry.coordinates[0],
             "depth": recent.geometry.coordinates[2],
-            "day": await new Date(recent.properties.time).getDate,
-            "month": await new Date(recent.properties.time).getMonth + 1,
+            "day": earthquakeTime.getDay() + 1,
+            "month": earthquakeTime.getMonth() + 1,
         }
     }
 
@@ -352,33 +390,118 @@ app.get('/app/risk', async (req, res) => {
 
     locals.predicted = earthquakes.data
 
+    locals.months = [
+        "",
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+
     if (isMobile(req, res)) {
         res.render('new/mobile/indexinfo.ejs', locals);
     } else {
         res.render('new/mobile/indexinfo.ejs', locals);
     }
-   
+
 });
 
-app.get('/app/plan', async (req, res) => {
+app.get("/app/plans", async (req, res) => {
+    var cookies = req.cookies;
+
+    if (req.cookies.userid == undefined || req.cookies.sessiontoken == undefined) {
+        res.send("Invalid User")
+        return
+    }
+
+    if (validateUser(cookies.userid, cookies.sessiontoken) == false) {
+        res.send("Invalid User")
+        return
+    }
+
+    var user = await getUser(cookies.userid);
+    var plans = user.plans;
+
+    console.log(plans)
+
     var locals = {
-    };
+
+    }
+
+    for (i = 0; i < plans.length; i++) {
+        var plan = plans[i];
+        var date = plan.created.toDate();
+
+        plans[i].createdatestring = `${Months[date.getMonth() + 1]} ${date.getDate()}, ${date.getFullYear()}`;
+    }
+    console.log(plans)
+    locals.plans = plans;
+
+    res.render('new/mobile/planning/plans.ejs', locals);
+
+});
+
+app.get("/app/plans/view/:id", async (req, res) => {
+    if (req.cookies.userid == undefined || req.cookies.sessiontoken == undefined) {
+        res.send("Invalid User")
+        return
+    }
+
+    if (validateUser(req.cookies.userid, req.cookies.sessiontoken) == false) {
+        res.send("Invalid User")
+        return
+    }
+
+    var user = await getUser(req.cookies.userid);
+    var plan = {};
+
+    for (i = 0; i < user.plans.length; i++) {
+        if (user.plans[i].id == req.params.id) {
+            plan = user.plans[i];
+            plan.createdatestring = `${Months[plan.created.toDate().getMonth() + 1]} ${plan.created.toDate().getDate()}, ${plan.created.toDate().getFullYear()}`;
+            plan.updatedstring = `${Months[plan.lastUpdated.toDate().getMonth() + 1]} ${plan.lastUpdated.toDate().getDate()}, ${plan.lastUpdated.toDate().getFullYear()}`;
+        }
+    }
+
+    if (plan.id == undefined) {
+        res.send("Invalid Plan")
+        return
+    }
+
+    var locals = {
+        "plan": plan
+    }
+
+    res.render('new/mobile/planning/plan.ejs', locals);
+});
+
+/* 
+app.get('/app/plans/select', async (req, res) => {
+    var locals = {};
 
     var ip = await req.ip;
     console.log(ip);
     if (ip.startsWith(":")) {
         ip = "128.12.123.204";
     }
-    
-    if (ip == "128.12.123.204"){
+
+    if (ip == "128.12.123.204") {
         locals.city = "Stanford";
         locals.region = "CA";
         locals.longitude = "-122.1639";
         locals.latitude = "37.423";
         locals.response = false
-    }else{
+    } else {
         var ipInfo = await getIPInfo(ip);
-        if (ipInfo.city){
+        if (ipInfo.city) {
             locals.city = ipInfo.city;
             locals.region = ipInfo.region_code;
             locals.longitude = ipInfo.longitude;
@@ -394,11 +517,7 @@ app.get('/app/plan', async (req, res) => {
     }
 })
 
-app.get("/app/plans/:name", async (req, res) => {
-    
-});
-
-app.get("/app/plan/:name", async (req, res) => {
+app.get("/app/plans/create/:name", async (req, res) => {
     var locals = {};
     locals.name = req.params.name;
 
@@ -412,11 +531,11 @@ app.get("/app/plan/:name", async (req, res) => {
 
     var found = true;
     var key = "";
-    while (found){
+    while (found) {
         key = createRandomString(10);
-        if (planCache[key] == undefined){
+        if (planCache[key] == undefined) {
             found = false;
-        }else{
+        } else {
             found = true;
         }
     }
@@ -462,7 +581,7 @@ app.get("/app/plan/:name/:planID", async (req, res) => {
 
     var plan = planCache[req.params.planID];
 
-    if (plan == undefined){
+    if (plan == undefined) {
         res.redirect("/app/plan/" + req.params.name);
         return;
     }
@@ -488,38 +607,12 @@ app.get("/app/plan/:name/:planID", async (req, res) => {
         res.render('new/mobile/planning/plan.ejs', locals);
     }
 });
-
-app.post("/api/plan/:jobid/update", async (req, res) => {
-    var plan = planCache[req.params.jobid];
-
-    if (plan == undefined){
-        res.send("not found");
-        return;
-    }
-
-    if (req.body.type == undefined){
-        res.send("invalid type");
-        return;
-    }
-
-    if (req.body.type == "action"){
-
-    }
-
-    var ip = await req.ip;
-    var ipInfo = await getIPInfo(ip);
-
-    plan.ip.push(await req.ip);
-    plan.lastUpdated = new Date();
-
-});
-
-/* 
+*/
+ 
 app.use(function(req, res, next) {
     res.redirect('/'); 
 });
 
-*/
 
 /*
 await sleep(0)
